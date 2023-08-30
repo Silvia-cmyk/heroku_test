d00670806bc6585b623ab055e355561f0cd8843d
shawny <seaforest01225@gmail.com>
2023-08-30 17:54:40 +0800

    particle cancel

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, {useState,useCallback, useMemo} from 'react';
+import React, {useState} from 'react';
 import questions from "./questionData";
 import './App.css'
 import Button from '@mui/material/Button';
-import Particles from "react-particles";
-import { loadSlim } from "tsparticles-slim";
 import {View, Image, StyleSheet, TextInput} from 'react-native';
 
 // 定義測驗題目數據，每個問題包含 question、options；移至另外的js檔省空間
 
 
 function App() {
-  const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
-        await loadSlim(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
     // 使用 useState 鉤子來管理當前題目和使用者答案
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
@@ -49,125 +35,6 @@ function App() {
         backgroundColor: 'rgba(255, 255, 255, 0.3)'
         },
       });
-    const particlesContainer = useMemo(() => (
-    <div className="particles-container">
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          "particles": {
-                  "number": {
-                    "value": 80,
-                    "density": {
-                      "enable": true,
-                      "value_area": 800
-                    }
-                  },
-                  "color": {
-                    "value": "#ffffff"
-                  },
-                  "shape": {
-                    "type": "star",
-                    "stroke": {
-                      "width": 0,
-                      "color": "#000000"
-                    },
-                    "polygon": {
-                      "nb_sides": 5
-                    },
-                    "image": {
-                      "src": "img/github.svg",
-                      "width": 100,
-                      "height": 100
-                    }
-                  },
-                  "opacity": {
-                    "value": 0.5,
-                    "random": false,
-                    "anim": {
-                      "enable": false,
-                      "speed": 1,
-                      "opacity_min": 0.1,
-                      "sync": false
-                    }
-                  },
-                  "size": {
-                    "value": 3,
-                    "random": true,
-                    "anim": {
-                      "enable": false,
-                      "speed": 40,
-                      "size_min": 0.1,
-                      "sync": false
-                    }
-                  },
-                  "line_linked": {
-                    "enable": true,
-                    "distance": 150,
-                    "color": "#ffffff",
-                    "opacity": 0.4,
-                    "width": 1
-                  },
-                  "move": {
-                    "enable": true,
-                    "speed": 4,
-                    "direction": "none",
-                    "random": false,
-                    "straight": false,
-                    "out_mode": "out",
-                    "bounce": false,
-                    "attract": {
-                      "enable": false,
-                      "rotateX": 600,
-                      "rotateY": 1200
-                    }
-                  }
-                },
-                "interactivity": {
-                  "detect_on": "canvas",
-                  "events": {
-                    "onhover": {
-                      "enable": true,
-                      "mode": "grab"
-                    },
-                    "onclick": {
-                      "enable": true,
-                      "mode": "push"
-                    },
-                    "resize": true
-                  },
-                  "modes": {
-                    "grab": {
-                      "distance": 400,
-                      "line_linked": {
-                        "opacity": 1
-                      }
-                    },
-                    "bubble": {
-                      "distance": 400,
-                      "size": 40,
-                      "duration": 2,
-                      "opacity": 8,
-                      "speed": 3
-                    },
-                    "repulse": {
-                      "distance": 200,
-                      "duration": 0.4
-                    },
-                    "push": {
-                      "particles_nb": 4
-                    },
-                    "remove": {
-                      "particles_nb": 2
-                    }
-                  }
-                },
-                "retina_detect": true
-        }}
-      />
-    </div>
-  ), []);
     // 處理選擇答案的事件，將答案添加到 userAnswers 中，並呼叫 submitAnswer 和 handleNextQuestion 函數
     const handleAnswerSelect = (selectedOption) => {
       setUserAnswers([...userAnswers, selectedOption]);
@@ -229,10 +96,6 @@ function App() {
   };
   return (
     <div>
-      <div className="background-layer">
-          {particlesContainer}
-      </div>
-
       <div className="quiz-container">
       {/* 判斷是否還有問題未作答 */}
       {currentQuestion < questions.length -1 ? (
