f9594338f6c16948c35ab824c83b22d74cd7a9d1
Sia <72331380+Silvia-cmyk@users.noreply.github.com>
2023-11-16 14:21:48 +0800

    Update questionData.js

--- src/questionData.js
+++ src/questionData.js
@@ -1,84 +1,8 @@
 const questions = [
     {
     question: '第一次見面一起去的地點？',
-    options: ['a.南紡', 'b.七股', 'c.奇美博物館'],
-    correctAnswer: 'c.奇美博物館',
-    },
-    {
-    question: '我的生日是？',
-    options: ['a.3月9日', 'b.11月13日', 'c.12月12日', 'd.6月30日'],
-    correctAnswer: 'c.12月12日',
-    },
-    {
-    question: '我們再聯絡的日期是？',
-    options: ['a.7月1日', 'b.7月19日', 'c.7月28日'],
-    correctAnswer: 'b.7月19日',
-    },
-    {
-    question: '曾經去七股路途中車上一直播的音樂最有印象的一首？',
-    options: ['a.膽小鬼', 'b.飛鳥與蟬'],
-    correctAnswer: 'a.膽小鬼',
-    },
-    {
-    question: '我們從哪個地方認識的？',
-    options: ['a.絕代雙驕', 'b.Line', 'c.Telegram'],
-    correctAnswer: 'a.絕代雙驕',
-    },
-    {
-    question: '我們認識至少有幾年了？',
-    options: ['a.4年', 'b.10年', 'c.1年'],
-    correctAnswer: 'a.4年',
-    },
-    {
-    question: '我喜歡的動物是什麼？',
-    options: ['a.貓咪', 'b.狗狗'],
-    correctAnswer: 'b.狗狗',
-    },
-    {
-    question: '這是哪裡？',
-    image:'seven_picture',
-    options: ['a.七股', 'b.奇美博物館','c.南紡'],
-    correctAnswer: 'a.七股',
-    },
-    {
-    question: '我最喜歡的顏色是什麼？',
-    options: ['a.黃色','b.紫色','c.黑色'],
-    correctAnswer: 'b.紫色'
-    },
-    {
-    question: '我最喜歡的食物是什麼？',
-    options: ['a.酪梨牛奶','b.牛肉麵','c.秋刀魚','d.以上皆是'],
-    correctAnswer: 'd.以上皆是'
-    },
-    {
-    question: '數字：79150101，倒過來乘以2並加30為多少？',
-    options: ['a.20230503','b.20230719','c.20210424','d.20220404'],
-    correctAnswer: 'c.20210424'
-    },
-    {
-    question: '我的星座是什麼？',
-    options: ['a.天秤座','b.射手座','c.天蠍座'],
-    correctAnswer: 'b.射手座'
-    },
-    {
-    question: '我的興趣是什麼？',
-    options: ['a.資安','b.遊戲開發','c.寫程式','d.以上皆是'],
-    correctAnswer: 'd.以上皆是'
-    },
-    {
-    question: '曾經你提過要一起去到水族館參觀，如果去到了水族館你最想看的是哪一種海洋生物表演？',
-    options: ['a.水母','b.海豚','c.企鵝','d.熱帶魚'],
-    correctAnswer: ''
-    },
-    {
-    question: '這是一串凱撒密碼:Dtz xnruqd fwj ymj gjxy. Mfaj ymj gjxy gnwymifd，其中每個字母都已經向後移動了五位，所以解密的答案是？',
-    options: ['a.You can\'t connect the dots looking forward; you can only connect them looking backwards.你無法預先把點點滴滴串連起來；只有在未來回顧時，你才會明白那些點點滴滴是如何串在一起的。','You simply are the best. Have the best birthday.你是最棒的。願你度過最棒的生日。','The darkness is no darkness with thee.有了你，黑暗不再是黑暗。'],
-    correctAnswer: 'You simply are the best. Have the best birthday.你是最棒的。願你度過最棒的生日。'
-    },
-    {
-    question: '最後一題',
-    options: ['不顯示'],
-    correctAnswer: ''
+    options: ['a.x', 'b.xx', 'c.xxx'],
+    correctAnswer: 'c.xxx',
     },
     
     // 在這裡添加更多題目
